80d0e61dc53dd029c309fae8d4899f4d1759d66f
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-03-15 12:00:28 +0000

    fix: one-liner the model missed

---
+++ Views/ProtocolProvider.swift
@@ -0,0 +1,14 @@
+// fix: one-liner the model missed
+import Foundation
+
+struct ProtocolProvider {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+    var field7: String = ""
+    var field8: String = ""
+}
