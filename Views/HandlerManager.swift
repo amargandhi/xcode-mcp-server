b79c2f04d0b5b070e394caf54519ff8ea73d9649
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-03-28 16:54:49 +0000

    fix: suppress the new Xcode warning from the beta

---
+++ Views/HandlerManager.swift
@@ -0,0 +1,9 @@
+// fix: suppress the new Xcode warning from the beta
+import Foundation
+
+struct HandlerManager {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
