7b1a583c4e353a3515ec1acb555b848cf756f97b
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-04-28 12:41:47 +0000

    adjust form spacing

---
+++ Services/StoreAdapter.swift
@@ -0,0 +1,10 @@
+// adjust form spacing
+import Foundation
+
+struct StoreAdapter {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+}
