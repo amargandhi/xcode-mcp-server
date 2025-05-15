7878e490649e9f09ad42fa2ba1846935d8ae8cb6
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-05-15 17:48:42 +0000

    remove stale import from 3D module [sonnet-3.7]

---
+++ Models/ViewManager.swift
@@ -0,0 +1,9 @@
+// remove stale import from 3D module [sonnet-3.7]
+import Foundation
+
+struct ViewManager {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
