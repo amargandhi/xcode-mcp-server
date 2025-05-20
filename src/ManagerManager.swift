806005374f3d30cc0283fbb648c85bc009bb3389
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-05-20 17:55:51 +0000

    missed a comma in the Combine declaration

---
+++ src/ManagerManager.swift
@@ -0,0 +1,9 @@
+// missed a comma in the Combine declaration
+import Foundation
+
+struct ManagerManager {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
