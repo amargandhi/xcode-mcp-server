c9312f7b3f65b6fdae882f31b2fbeb4a8ffb48da
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-05-20 16:49:23 +0000

    add missing return type to chain function

--- Models/ManagerEngine.swift
+++ Models/ManagerEngine.swift
@@ -4,4 +4,11 @@ import Foundation
 struct ManagerEngine {
     var field0: String = ""
     var field1: String = ""
-}
+// add missing return type to chain function
+    // line 0
+    // line 1
+    // line 2
+    // line 3
+    // line 4
+    // line 5
+    // line 6
