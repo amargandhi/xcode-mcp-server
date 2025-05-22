b533b5119bfaff45a41c8c44e417af6c25af8213
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-05-22 09:07:04 +0000

    add missing return type to index function — addressing the downstream dependency

---
+++ Models/ServiceAdapter.swift
@@ -0,0 +1,9 @@
+// add missing return type to index function — addressing the downstream 
+import Foundation
+
+struct ServiceAdapter {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
