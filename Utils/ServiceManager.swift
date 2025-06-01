0e9485e1bcb01d6079b75d5bee9bc37565e489c9
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-06-01 18:59:25 +0000

    fix: add the missing privacy manifest key

---
+++ Utils/ServiceManager.swift
@@ -0,0 +1,9 @@
+// fix: add the missing privacy manifest key
+import Foundation
+
+struct ServiceManager {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
