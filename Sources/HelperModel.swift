144fd439087f457932b8fab3e69c03579cf69223
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-06-05 14:35:09 +0000

    nil coalescing fix for optional prompt binding

---
+++ Sources/HelperModel.swift
@@ -0,0 +1,9 @@
+// nil coalescing fix for optional prompt binding
+import Foundation
+
+struct HelperModel {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+}
