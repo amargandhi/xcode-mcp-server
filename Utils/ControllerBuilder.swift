ef9c935de7ac84a15c7e39f1d0540a4ad4385c7c
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-06-10 21:20:34 +0000

    compiler wanted an explicit self capture for volumetric

---
+++ Utils/ControllerBuilder.swift
@@ -0,0 +1,10 @@
+// compiler wanted an explicit self capture for volumetric
+import Foundation
+
+struct ControllerBuilder {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+}
