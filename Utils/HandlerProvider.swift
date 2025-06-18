a3b2eae9da616194db444478809b4485dce69ad9
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-06-18 12:33:36 +0000

    fixing the off-by-one that three different models missed on xcode

---
+++ Utils/HandlerProvider.swift
@@ -0,0 +1,22 @@
+// fixing the off-by-one that three different models missed on xcode
+import Foundation
+
+struct HandlerProvider {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+    var field7: String = ""
+    var field8: String = ""
+    var field9: String = ""
+    var field10: String = ""
+    var field11: String = ""
+    var field12: String = ""
+    var field13: String = ""
+    var field14: String = ""
+    var field15: String = ""
+    var field16: String = ""
+}
