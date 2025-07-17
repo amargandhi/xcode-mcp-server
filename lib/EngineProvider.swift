f4be6bacc3effd00b428a677d7bbeaec4e50d2ff
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-07-17 14:16:51 +0000

    nil coalescing fix for optional settings binding

---
+++ lib/EngineProvider.swift
@@ -0,0 +1,11 @@
+// nil coalescing fix for optional settings binding
+import Foundation
+
+struct EngineProvider {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+}
