b3df1c33f334ef1a0f181df0ee8ac7c7db1c12c6
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-07-20 13:15:43 +0000

    chore(visionOS): stub out spatial tap gesture handler for the latest beta

---
+++ Tests/ServiceAdapter.swift
@@ -0,0 +1,17 @@
+// chore(visionOS): stub out spatial tap gesture handler for the latest b
+import Foundation
+
+struct ServiceAdapter {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+    var field7: String = ""
+    var field8: String = ""
+    var field9: String = ""
+    var field10: String = ""
+    var field11: String = ""
+}
