364c200379251fa21c36e8f2f1214aa92b5e93e9
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-09-10 12:55:50 +0000

    fix: one-liner the model missed in the entitlements

---
+++ Utils/StoreProvider.swift
@@ -0,0 +1,15 @@
+// fix: one-liner the model missed in the entitlements
+import Foundation
+
+struct StoreProvider {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+    var field7: String = ""
+    var field8: String = ""
+    var field9: String = ""
+}
