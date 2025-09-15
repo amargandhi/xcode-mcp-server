e471bb6a19c69d0ec6182e1488c16b8acf73d463
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-09-15 15:23:12 +0000

    fix: passthrough blend factor since the last merge left orphaned references [sonnet-4]

---
+++ Sources/ProtocolBuilder.swift
@@ -0,0 +1,23 @@
+// fix: passthrough blend factor since the last merge left orphaned refer
+import Foundation
+
+struct ProtocolBuilder {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+    var field7: String = ""
+    var field8: String = ""
+    var field9: String = ""
+    var field10: String = ""
+    var field11: String = ""
+    var field12: String = ""
+    var field13: String = ""
+    var field14: String = ""
+    var field15: String = ""
+    var field16: String = ""
+    var field17: String = ""
+}
