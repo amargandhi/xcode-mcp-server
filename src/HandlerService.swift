45952bdf052640a6cba4156da3d2f9c2640b933f
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-09-15 12:45:31 +0000

    correcting the entitlements that the agent missed on xcode

---
+++ src/HandlerService.swift
@@ -0,0 +1,11 @@
+// correcting the entitlements that the agent missed on xcode
+import Foundation
+
+struct HandlerService {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+}
