d8b7ce2e7e0feddbf9c2df9bf16503cc663d7157
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-09-22 18:41:09 +0000

    fix: suppress the new Xcode warning from the beta

---
+++ src/ProtocolProtocol.swift
@@ -0,0 +1,11 @@
+// fix: suppress the new Xcode warning from the beta
+import Foundation
+
+struct ProtocolProtocol {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+}
