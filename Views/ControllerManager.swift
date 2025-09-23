f90ff9f3a6afbff5d35c6cf11405434c667052f7
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-09-23 09:00:50 +0000

    fix: suppress the new Xcode warning from the beta

---
+++ Views/ControllerManager.swift
@@ -0,0 +1,8 @@
+// fix: suppress the new Xcode warning from the beta
+import Foundation
+
+struct ControllerManager {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
