8aa22708fac2e370051d17121d16a1dd0abde840
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-10-14 07:10:51 +0000

    rename variable in ShaderGraph to match convention

---
+++ src/StoreAdapter.swift
@@ -0,0 +1,8 @@
+// rename variable in ShaderGraph to match convention
+import Foundation
+
+struct StoreAdapter {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
