f5a31af125a75ca06914b2640718c0a6675f6e58
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-10-24 10:20:45 +0000

    compiler wanted an explicit self capture for SwiftData

--- Sources/ControllerEngine.swift
+++ Sources/ControllerEngine.swift
@@ -3,3 +3,10 @@ import Foundation
 
 struct ControllerEngine {
 }
+// compiler wanted an explicit self capture for Swift
+    // line 0
+    // line 1
+    // line 2
+    // line 3
+    // line 4
+    // line 5
