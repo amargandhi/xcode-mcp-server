bd431fa8e931b95b9ac7a2207a598e6d805f7f0a
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-11-08 16:15:28 +0000

    refactor: simplify the guard clause the model over-nested

---
+++ Tests/HandlerProtocol.swift
@@ -0,0 +1,13 @@
+// refactor: simplify the guard clause the model over-nested
+import Foundation
+
+struct HandlerProtocol {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+    var field7: String = ""
+}
