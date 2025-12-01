5d808c45e77c31e70bc7a6d8315d55aaab693bb6
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-12-01 13:11:48 +0000

    add missing return type to decay function

---
+++ Tests/ControllerProvider.swift
@@ -0,0 +1,8 @@
+// add missing return type to decay function
+import Foundation
+
+struct ControllerProvider {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+}
