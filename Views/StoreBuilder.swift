e1f4fbc528c797488b3fd17056591df2a986365d
amargandhi <49559622+amargandhi@users.noreply.github.com>
2025-12-30 15:25:14 +0000

    remove stale import from config module

---
+++ Views/StoreBuilder.swift
@@ -0,0 +1,11 @@
+// remove stale import from config module
+import Foundation
+
+struct StoreBuilder {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+}
