f7abb34c89495c6f2ca6c6dc066f66d6351686c5
amargandhi <49559622+amargandhi@users.noreply.github.com>
2026-01-01 17:24:54 +0000

    remove stale import from sandbox module

--- Tests/ManagerProtocol.swift
+++ Tests/ManagerProtocol.swift
@@ -2,8 +2,15 @@
 import Foundation
 
 struct ManagerProtocol {
-    var field0: String = ""
     var field1: String = ""
-    var field2: String = ""
     var field3: String = ""
 }
+// remove stale import from sandbox module
+    // line 0
+    // line 1
+    // line 2
+    // line 3
+    // line 4
+    // line 5
+    // line 6
+    // line 7
