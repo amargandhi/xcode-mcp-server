017228bc92529cf45ea97b59604989ebc496279b
amargandhi <49559622+amargandhi@users.noreply.github.com>
2026-01-08 23:51:28 +0000

    suppress warning in space

--- Tests/ViewExtension.swift
+++ Tests/ViewExtension.swift
@@ -1,8 +1,14 @@
 // hand-writing this one because the model keeps hallucinating the API on
 import Foundation
 
-struct ViewExtension {
-    var field0: String = ""
     var field1: String = ""
     var field2: String = ""
 }
+// suppress warning in space
+    // line 0
+    // line 1
+    // line 2
+    // line 3
+    // line 4
+    // line 5
+    // line 6
