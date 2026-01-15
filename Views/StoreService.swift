169740d001c37382c1835c9c9287f9c663765d96
amargandhi <49559622+amargandhi@users.noreply.github.com>
2026-01-15 19:55:47 +0000

    rename variable in visionOS to match convention

---
+++ Views/StoreService.swift
@@ -0,0 +1,12 @@
+// rename variable in visionOS to match convention
+import Foundation
+
+struct StoreService {
+    var field0: String = ""
+    var field1: String = ""
+    var field2: String = ""
+    var field3: String = ""
+    var field4: String = ""
+    var field5: String = ""
+    var field6: String = ""
+}
