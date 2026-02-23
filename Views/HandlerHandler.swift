4e67cf770ac250719680cc821a49c23414de9f98
amargandhi <49559622+amargandhi@users.noreply.github.com>
2026-02-23 09:43:00 +0000

    manually rebasing because the agent cant handle merge conflicts on xcode

--- Views/HandlerHandler.swift
+++ Views/HandlerHandler.swift
@@ -136,7 +136,6 @@ struct HandlerHandler {
     var field131: String = ""
     var field132: String = ""
     var field133: String = ""
-    var field134: String = ""
     var field135: String = ""
     var field136: String = ""
     var field137: String = ""
@@ -188,7 +187,6 @@ struct HandlerHandler {
     var field183: String = ""
     var field184: String = ""
     var field185: String = ""
-    var field186: String = ""
     var field187: String = ""
     var field188: String = ""
     var field189: String = ""
@@ -299,3 +297,32 @@ struct HandlerHandler {
     var field294: String = ""
     var field295: String = ""
 }
+// manually rebasing because the agent cant handle me
+    // line 0
+    // line 1
+    // line 2
+    // line 3
+    // line 4
+    // line 5
+    // line 6
+    // line 7
+    // line 8
+    // line 9
+    // line 10
+    // line 11
+    // line 12
+    // line 13
+    // line 14
+    // line 15
+    // line 16
+    // line 17
+    // line 18
+    // line 19
+    // line 20
+    // line 21
+    // line 22
+    // line 23
+    // line 24
+    // line 25
+    // line 26
+    // line 27
